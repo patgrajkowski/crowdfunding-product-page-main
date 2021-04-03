e2936cce49f2f054d1508cf522f31c734a5acd3b
Patryk Grajkowski <patryk@patryk-pc.home>
2021-04-04 00:33:48 +0200

    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburgerButton = document.getElementById('hamburger-button');
+const wrapper = document.getElementsByClassName('wrapper')[0];
+const hamburgerMenu = document.getElementById('menu');
+const closeMenu = (event) => {
+    if(event.target === hamburgerMenu){
+        hamburgerMenu.style.display = 'none';
+        wrapper.style.position = '';
+    }
+};
+const toggleMenu = () => {
+    console.log('xd');
+    if(hamburgerMenu.style.display === '' || hamburgerMenu.style.display === 'none'){
+        hamburgerMenu.style.display = 'flex';
+        wrapper.style.position = 'fixed';
+    }else{
+        hamburgerMenu.style.display = 'none';
+        wrapper.style.position = '';
+    }
+};
+hamburgerButton.addEventListener('click',toggleMenu);
+window.addEventListener('click', closeMenu);
