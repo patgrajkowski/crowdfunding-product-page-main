4efc0df63e721124238e1fb8761d576eb1d91c16
Patryk Grajkowski <patryk@patryk-pc.home>
2021-04-05 13:21:40 +0200

    commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const hamburgerButton = document.getElementById('hamburger-button');
 const wrapper = document.getElementsByClassName('wrapper')[0];
 const hamburgerMenu = document.getElementById('menu');
+const bookmarkButton = document.getElementById('bookmark-button');
+let bookmarked = false;
+const bookmarkImg = document.getElementById('bookmark-img');
 const closeMenu = (event) => {
     if(event.target === hamburgerMenu){
         hamburgerMenu.style.display = 'none';
@@ -10,9 +13,10 @@ const closeMenu = (event) => {
 };
 const toggleMenu = () => {
     if(hamburgerMenu.style.display === ''){
-        hamburgerMenu.style.display = 'flex';
+        hamburgerMenu.style.toggle = 'flex';
         wrapper.style.position = 'fixed';
         hamburgerButton.src = './images/icon-close-menu.svg'
+
     }else{
         hamburgerMenu.style.display = '';
         wrapper.style.position = '';
@@ -20,5 +24,20 @@ const toggleMenu = () => {
 
     }
 };
+const toggleBookmark = () => {
+    console.log('xd');
+    if(!bookmarked){
+        bookmarkButton.classList.add('bookmarked');
+        bookmarkButton.innerHTML = '<img src="./images/icon-bookmarked.svg" alt="icon-bookmark" id="bookmarked-img">Bookmarked';
+        bookmarkButton.style.paddingRight = '1.5rem';
+        bookmarked = true;
+    }else{
+        bookmarkButton.classList.remove('bookmarked');
+        bookmarkButton.innerHTML = '<img src="./images/icon-bookmark.svg" alt="icon-bookmark" id="bookmark-img">Bookmark'
+        bookmarkButton.style.paddingRight = '';
+        bookmarked = false;
+    }
+}
 hamburgerButton.addEventListener('click',toggleMenu);
 window.addEventListener('click', closeMenu);
+bookmarkButton.addEventListener('click', toggleBookmark);
